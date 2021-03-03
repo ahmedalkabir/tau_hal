3ae020395b39d070d452d89d10e37c7d3c2173bc
Ahmed Alkabir <korabekaali@outlook.com>
2021-03-03 23:31:13 +0200

    implement system timer

--- src/tau_avr.c
+++ src/tau_avr.c
@@ -22,3 +22,12 @@ void tau_write_gpio(volatile uint8_t *port, uint8_t pin, pin_status value){
 pin_status tau_read_gpio(volatile uint8_t *port, uint8_t pin){
     return (pin_status)(*(port-1) & (1 << pin));   
 }
+
+void system_clock_init(){
+    TCCR0A |= (1 << WGM01); // CTC mode
+    TCCR0B |= (1 << CS00) | (1 << CS01); // CPU_CLOCK / 64
+    TIMSK0 |= (1 << OCIE0A);  // enable compare match A interrupt
+    // as it will be called every 1ms
+    TCNT0 = 0;
+    OCR0A = 249;
+}
